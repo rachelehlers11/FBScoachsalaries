9b1f115519a2c8b14eb4642ae6a31450f514e463
Rachel Ehlers <rachelehlers@Rachels-MacBook-Pro.local>
2017-11-11 00:45:25 -0700

    initial commit

--- dataframebuilder.py
+++ dataframebuilder.py
@@ -12,41 +12,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# First function scrapes the first table from the page.
+# The ifelse statements in the makedataframe function are 
+# specific to these web pages as well as the NaN replacements.
+
+
 def makedataframe(link): 
     r = requests.get(link)
     soup = BeautifulSoup(r.content, 'lxml')
     table = soup.find_all('table')
-    name = pd.read_html(str(table),encoding='utf-8')
-    if len(name) > 1:
-        name = name[0].join(name[1], how = 'outer', on = None )
+    data = pd.read_html(str(table),encoding='utf-8')
+    # ifelse to handle the waybackmachine's different table output
+    if len(data) > 1:
+        data = data[0].join(data[1], how = 'outer', on = None )
     else:
-        name = name[0]
-    name = name.replace('--', np.NaN)
-    return name
+        data = data[0]
+    data = data.replace('--', np.NaN) # null values in the table are '--'
+    return data
+
+
+
 #make the dataframes
-#the extra index is required on 2016, 2017 dataframes to remove the table headers at the bottom of the table whose purpose is visual assistance 
+
+# The extra removal of the last line, "[:-1]" , is required on 2016, 2017 dataframes to 
+# remove the table headers at the bottom of the table whose purpose is visual astistance 
 
 #head coaches
-hclink = 'http://sports.usatoday.com/ncaa/salaries/'
-fourteen = makedataframe('https://web.archive.org/web/20141230041628/' + hclink)
-fifteen = makedataframe('https://web.archive.org/web/20151214221657/' + hclink)
-sixteen = makedataframe('https://web.archive.org/web/20161215053646/' + hclink)
-seventeen = makedataframe(hclink)[:-1]
+headcoachlink = 'http://sports.usatoday.com/ncaa/salaries/'
+fourteen = makedataframe('https://web.archive.org/web/20141230041628/' + headcoachlink)
+fifteen = makedataframe('https://web.archive.org/web/20151214221657/' + headcoachlink)
+sixteen = makedataframe('https://web.archive.org/web/20161215053646/' + headcoachlink)
+seventeen = makedataframe(headcoachlink)[:-1] # Remove extra table header at the bottom.
 
 #assistant coaches
-aclink = 'http://sports.usatoday.com/ncaa/salaries/football/assistant'
-assfourteen = makedataframe('https://web.archive.org/web/20150129211154/' + aclink)
-assfifteen = makedataframe('https://web.archive.org/web/20160130233153/' + aclink)
-asssixteen = makedataframe(aclink)[:-1]
+assistantlink = 'http://sports.usatoday.com/ncaa/salaries/football/assistant'
+astfourteen = makedataframe('https://web.archive.org/web/20150129211154/' + assistantlink)
+astfifteen = makedataframe('https://web.archive.org/web/20160130233153/' + assistantlink)
+astsixteen = makedataframe(assistantlink)[:-1] # Remove extra table header at the bottom.
 
 #strengthcoaches
-strengthsixteen = makedataframe('http://sports.usatoday.com/ncaa/salaries/football/strength')[:-1]
+strengthcoachlink = 'http://sports.usatoday.com/ncaa/salaries/football/strength'
+strengthsixteen = makedataframe(strengthcoachlink)[:-1] # Remove extra table header at the bottom.
 
 
 
 
-# now look at the column names, either on the site or by calling df.columns. 
-# change for uniformity/simplicity
+# Change column names for uniformity. Different lengths and different names,
+# so had to be done manually 
+
 fourteen.columns = ['rank', 'school', 'conf', 'headcoach', 'schoolpay',\
                     'otherpay', 'totalpay', 'maxbonus', 'stafftotal']
 
@@ -61,33 +74,42 @@ sixteen.columns = ['rank', 'school', 'conf', 'headcoach', 'schoolpay',\
 seventeen.columns = ['rank', 'school', 'conf', 'headcoach', 'schoolpay',\
                      'totalpay', 'maxbonus', 'bonusespaid', 'stafftotal', \
                      'buyout']
-assfourteen.columns = ['rank', 'school', 'conf', 'asscoach', 'schoolpay',\
+astfourteen.columns = ['rank', 'school', 'conf', 'astcoach', 'schoolpay',\
                        'otherpay', 'totalpay', 'maxbonus', 'stafftotal']
 
-assfifteen.columns = ['rank', 'school', 'conf', 'asscoach', 'schoolpay',\
+astfifteen.columns = ['rank', 'school', 'conf', 'astcoach', 'schoolpay',\
                       'otherpay', 'totalpay', 'maxbonus', 'stafftotal']
 
-asssixteen.columns = ['rank', 'school', 'conf', 'asscoach', 'schoolpay',\
+astsixteen.columns = ['rank', 'school', 'conf', 'astcoach', 'schoolpay',\
                       'totalpay', 'maxbonus', 'stafftotal','buyout']
 
-strengthsixteen.columns = ['rank', 'school', 'conf', 'asscoach', 'schoolpay',\
+strengthsixteen.columns = ['rank', 'school', 'conf', 'astcoach', 'schoolpay',\
                            'totalpay', 'maxbonus' ,'stafftotal', 'buyout'] 
 
 
 #list of dataframes for reference in loops
-dflist = [fourteen, fifteen, sixteen, seventeen, assfourteen, assfifteen, \
-          asssixteen, strengthsixteen]
+dflist = [fourteen, fifteen, sixteen, seventeen, astfourteen, astfifteen, \
+          astsixteen, strengthsixteen]
 
 
-# clean the data 
+# Now, want to make the money data useful by removing the symbols. While
+# looping through each, item, might as well add another variable "category", which
+# tells whether the school is a Power Five or not. Made a string so I wouldn't have
+# to change plot labels in ggplot 
+
+
 numericcolumns = ['rank', 'schoolpay', 'otherpay', 'totalpay', 'maxbonus', \
                   'bonusespaid', 'stafftotal']
-powerfive = ['ACC', 'SEC', 'Big Ten', 'Big 12', 'Pac-12']
+powerfive = ['ACC', 'SEC', 'Big Ten', 'Big 12', 'PAC-12']
+
+
 for item in dflist:
     for x in item.columns:
         item[x] = item[x].astype(str).str.replace('$', '')
         item[x] = item[x].astype(str).str.replace(',', '')
     item['category'] = ['Power Five' if x in powerfive else 'Others' for x in item['conf']]
+
+#change the columns calculations will be made on to numeric.
     
 for item in dflist:    
     for x in item.columns: 
@@ -96,13 +118,42 @@ for item in dflist:
 
 
 
+# Option: export to CSV. If any changes are made re-write them
+
+os.chdir('FAKEPATH/ncaafootballsalaries2016/salaryfiles')
+f = '~/ncaafootballsalaries2016/salaryfiles'
+file = open('fourteen.txt', 'w', encoding = 'utf-8')
+file.write(fourteen.to_csv(path = f, index = False))
+file.close()
+
+file = open('fifteen.txt', 'w', encoding = 'utf-8')
+file.write(fifteen.to_csv(path = f, index = False))
+file.close()
 
 
+file = open('sixteen.txt', 'w', encoding = 'utf-8')
+file.write(sixteen.to_csv(path = f, index = False))
+file.close()
 
+file = open('seventeen.txt', 'w', encoding = 'utf-8')
+file.write(seventeen.to_csv(path = f, index = False))
+file.close()
 
+file = open('astfourteen.txt', 'w', encoding = 'utf-8')
+file.write(astfourteen.to_csv(path = f, index = False))
+file.close()
 
+file = open('astfifteen.txt', 'w', encoding = 'utf-8')
+file.write(astfifteen.to_csv(path = f, index = False))
+file.close()
 
+file = open('astsixteen.txt', 'w', encoding = 'utf-8')
+file.write(astsixteen.to_csv(path = f, index = False))
+file.close()
 
+file = open('strengthsixteen.txt', 'w', encoding = 'utf-8')
+file.write(strengthsixteen.to_csv(path = f, index = False))
+file.close()
 
 
 
